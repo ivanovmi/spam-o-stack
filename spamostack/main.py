97b37be3ae18950c416544328a8ec76976981eff
Mikhail Ivanov <mivanov@mirantis.com>
2016-09-20 16:25:34 +0300

    Initial commit

---
+++ spamostack/main.py
@@ -0,0 +1,12 @@
+import aodhclient
+import ceilometerclient
+import cinderclient
+import heatclient
+import glanceclient
+import keystoneauth1
+import keystoneclient
+import neutronclient
+import novaclient
+import openstackclient
+import swiftclient
+
