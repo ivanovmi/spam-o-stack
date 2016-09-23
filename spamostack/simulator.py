f1b4be3ee4e5ca3c8a5c441ff7788a213b0d8997
Daniil Trishkin <dtrishkin@mirantis.com>
2016-09-23 12:53:32 +0300

    In progress

--- spamostack/simulator.py
+++ spamostack/simulator.py
@@ -1,12 +1,24 @@
 import time
 from random import randint
+import threading
+from collections import OrderedDict
+
+from session import Session
+from client_factory import ClientFactory
+
+
+def threader(func):
+    def wrapper(self, *args, **kwargs):
+        threading.Thread(target=func, args=(*args, **kwargs)).start()
 
 
 class Simulator(object):
-    def __init__(self, client, pipeline):
+    def __init__(self, pipeline):
         self.client = client
         self.pipeline = pipeline
+        self.session = Session()
 
+    @threader
     def simulate(self):
         '''Simulate an actions'''
 
