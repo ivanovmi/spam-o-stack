e6352e152ebee48e6b29afb372f927495f4d9a61
Mikhail Ivanov <mivanov@mirantis.com>
2016-09-26 16:58:36 +0300

    Update cache insert/load

--- spamostack/cache.py
+++ spamostack/cache.py
@@ -32,25 +32,6 @@ class Cache(object):
         self.cache["created"]["users"][uname]['project_domain_id'] = 'default'
         self.cache["created"]["users"][uname]['user_domain_id'] = 'default'
 
-    def str_to_obj(self, client_name, cache_elem):
-        """
-        Convert string into object
-
-        @param client_name: Name of the component
-        @type client_name: `str`
-        @param cache_elem: String interpretation of the object to get
-        @type client_name: `str`
-        """
-
-        client = getattr(self.client_factory, client_name)
-        for i in cache_elem.split(','):
-            if i.startswith("id="):
-                id = i.split('=')[1]
-
-        obj = client.get(id=id)
-
-        return obj
-
     def load(self):
         """Load db into cache"""
 
@@ -59,8 +40,8 @@ class Cache(object):
                 if isinstance(value, dict):
                     to_objects(value, client_name)
                 else:
-                    cache_elem[key] = [self.str_to_obj(client_name, el)
-                                    for el in value]
+                    cache_elem[key] = [getattr(self.client_factory, client_name).get(id=el)
+                                       for el in value]
 
         for key, value in self.db.RangeIter():
             to_objects(eval(value), key)
@@ -71,9 +52,9 @@ class Cache(object):
         def to_strings(cache_elem):
             for key, value in cache_elem.iteritems():
                 if isinstance(value, dict):
-                    to_objects(value)
+                    to_strings(value)
                 else:
-                    cache_elem[key] = [str(el) for el in value]
+                    cache_elem[key] = [str(el.id) for el in value]
 
         batch = leveldb.WriteBatch()
         for key, value in self.cache.iteritems():
